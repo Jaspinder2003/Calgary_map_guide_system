422f03546b789e3e65dad587f8732e9bc9889832
Jaspinder2003 <jaspindersinghm860@gmail.com>
2025-04-22 23:33:49 -0600

    Update settings.py

--- CPSC471CalgaryGuidesystem/settings.py
+++ CPSC471CalgaryGuidesystem/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
-
+import platform
 from pathlib import Path
 
 if platform.system() == 'Windows':
