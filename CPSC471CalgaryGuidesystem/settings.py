7277ae06d788b6c6a42c183516f87e06c73f7208
Jaspinder2003 <jaspindersinghm860@gmail.com>
2025-05-12 11:04:32 -0600

    Update settings.py

--- CPSC471CalgaryGuidesystem/settings.py
+++ CPSC471CalgaryGuidesystem/settings.py
@@ -82,14 +82,15 @@ WSGI_APPLICATION = 'CPSC471CalgaryGuidesystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'CalgaryGuideSystem',
-        'USER': 'root',
-        'PASSWORD': '12345678',
-        'HOST': '34.130.21.78',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
